12d1c9794573d5ab21f3b9d321f02ad8d8104a60
Jeff Tingey <jefftingey22@gmail.com>
2016-12-26 00:14:42 -0700

    demo html, javascript, css.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^butt$', views.ButtView.as_view(), name='butt'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def index(request):
         return HttpResponse("Hello, world. You' deserve a punch in my nuts.")
 
 # Create your views here.
+
+class ButtView(TemplateView):
+    template_name = 'butt.html'
